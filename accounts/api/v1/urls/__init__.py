b288af320ecca7a799d12c4eceef1df06b6cd516
Mahziar <93269144+mahziarAhmadian@users.noreply.github.com>
2024-07-20 14:36:03 +0330

    add jwt verify and refresh api .

--- accounts/api/v1/urls/__init__.py
+++ accounts/api/v1/urls/__init__.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("accounts.api.v1.urls.user_urls")),
     path('profile/', include("accounts.api.v1.urls.profile_urls")),
+    path('jwt/', include("accounts.api.v1.urls.jwt_urls")),
 ]
